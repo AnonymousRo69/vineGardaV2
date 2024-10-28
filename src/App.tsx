fe154515f9c7f2262694d4ca0883bc5bfbc2bbdd
AnonymousRo69 <71792899+AnonymousRo69@users.noreply.github.com>
2024-10-28 16:08:09 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  let [Background, setBackground] = useState("#242424");
   let [Input, setInput] = useState("");
   let [Database, setDatabase] = useState<string[]>([]);
-  let [Found, setFound] = useState<null | boolean>(null);
+  let [FeedBack, setFeedBack] = useState<string>(
+    "Introdu un Numar de Imatriculare"
+  );
+  let [ShowLogin, setShowLogin] = useState(false);
+  let [Password, setPassword] = useState("");
   // let [Alive, setAlive] = useState<null | boolean>(null);
 
   async function GetDatabase() {
@@ -14,8 +19,7 @@ function App() {
       );
       if (response.ok) {
         const data = await response.json(); // Parse the response body as JSON
-        setDatabase(data); // Update the state with the parsed data
-        console.log(`Response Status: ${response.status}`); // Log the data for debugging
+        setDatabase(data); // Update the state with the parsed data // Log the data for debugging
       }
     } catch (error) {}
   }
@@ -23,43 +27,153 @@ function App() {
   //   GetDatabase();
   // }, []);
   const checkInputInDatabase = async () => {
-    await GetDatabase();
-    const isFound = Database.includes(Input.toLowerCase()); // Check if input exists in database
-    setFound(isFound);
-    console.log(isFound);
+    if (Input) {
+      await GetDatabase();
+      const isFound = Database.includes(Input.toLowerCase());
+      if (isFound) {
+        setFeedBack("Numarul de Imatriculare apartine unui Politist");
+      } else {
+        setFeedBack("Numarul nu se afla in baza de date");
+      }
+    }
+  };
+  const addToDatabase = async () => {
+    try {
+      let response = await fetch(
+        "https://bazadedatepd.up.railway.app/inputdatabase",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ numar: Input, token: Password }),
+        }
+      );
+      switch (response.status) {
+        case 200:
+          setShowLogin(false);
+          setFeedBack("Numarul a fost adaugat cu succes");
+          setTimeout(() => {
+            setFeedBack("Introdu un Numar de Imatriculare");
+          }, 2500);
+          return;
+        case 403:
+          setFeedBack("Parola Incorecta");
+          setTimeout(() => {
+            setFeedBack("Introdu un Numar de Imatriculare");
+          }, 2500);
+          return;
+        case 409:
+          setFeedBack("Numarul deja se afla in baza de date");
+          setTimeout(() => {
+            setFeedBack("Introdu un Numar de Imatriculare");
+          }, 2500);
+          return;
+        case 422:
+          setFeedBack("Numarul nu este Valid");
+          setTimeout(() => {
+            setFeedBack("Introdu un Numar de Imatriculare");
+          }, 2500);
+          return;
+      }
+    } catch (error) {}
   };
   return (
-    <div className="card">
-      <h1 className="title">Vine Garda?</h1>
-      <input
-        type="text"
-        name="numar"
-        id="numar"
-        placeholder="Cauta un Numar"
-        value={Input}
-        onChange={(event) => {
-          setInput(event.target.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          checkInputInDatabase();
-          setTimeout(() => {
-            setFound(null);
-          }, 1500);
-        }}
-      >
-        Cauta
-      </button>
-      {Found != null ? (
-        Found === true ? (
-          <div>E Gabor</div>
-        ) : (
-          <div>Nu e Gabor</div>
-        )
+    <div>
+      {ShowLogin ? (
+        <div
+          className="login"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "start",
+          }}
+        >
+          <button
+            onClick={() => {
+              setShowLogin(false);
+            }}
+          >
+            X
+          </button>
+          <div
+            style={{
+              paddingBottom: "20px",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            <h1 className="logintitle">Login</h1>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <input
+                style={{ width: "80%" }}
+                type="password"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+              <button
+                className="button"
+                onClick={() => {
+                  addToDatabase();
+                }}
+              >
+                Adauga
+              </button>
+            </div>
+          </div>
+        </div>
       ) : (
-        <div>Introdu un Numar de Imatriculare</div>
+        ""
       )}
+      <div
+        className="card animate"
+        style={{ backgroundColor: Background, padding: "20px" }}
+      >
+        <h1 className="title">Vine Garda?</h1>
+        <input
+          onAnimationEnd={() => {
+            setBackground("");
+          }}
+          type="text"
+          name="numar"
+          id="numar"
+          placeholder="Cauta un Numar"
+          value={Input}
+          onChange={(event) => {
+            setInput(event.target.value);
+          }}
+        />
+        <div style={{ margin: "10px" }} className="Buttons">
+          <button
+            style={{ margin: "10px" }}
+            onClick={() => {
+              setShowLogin(true);
+            }}
+          >
+            Adauga
+          </button>
+          <button
+            onClick={() => {
+              checkInputInDatabase();
+              setTimeout(() => {
+                setFeedBack("Introdu un Numar de Imatriculare");
+              }, 2500);
+            }}
+          >
+            Cauta
+          </button>
+        </div>
+        <div>{FeedBack}</div>
+      </div>
     </div>
   );
 }
